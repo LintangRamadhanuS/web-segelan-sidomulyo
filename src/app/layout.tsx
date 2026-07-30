9100b7eb5ee06e1e168504e3a2fb8ab4e1b7c41d
LintangRamadhanuS <dhanusetyawan10@gmail.com>
2026-07-30 09:37:14 +0700

    add: vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import { dusunInfo } from "@/lib/data";
+import { Analytics } from "@vercel/analytics/next"
 
 const inter = Inter({
   subsets: ["latin"],
@@ -50,6 +51,7 @@ export default function RootLayout({
         <Navbar />
         <main>{children}</main>
         <Footer />
+        <Analytics />
       </body>
     </html>
   );
